e8f123d47e97b73d573a7071fd3ff3805c2ff7ab
TrueBenja <benjaminstoreboe@gmail.com>
2024-11-03 22:40:38 +0100

    Fremgang

--- src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
@@ -4,12 +4,32 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
 import no.hvl.dat100.common.TODO;
+import no.hvl.dat100.oppgave1.Innlegg;
 import no.hvl.dat100.oppgave3.*;
 
 public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
+		boolean completed = false;
 
-		throw new UnsupportedOperationException(TODO.method());
+		PrintWriter writer = null;
+
+		Innlegg[] innleggTab = samling.getSamling();
+
+		try {
+			writer = new PrintWriter(mappe + filnavn);
+			for (Innlegg e : innleggTab) {
+				writer.println(e.toString());
+			}
+			completed = true;
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			if (writer != null) {
+				writer.close();
+			}
+		}
+
+		return completed;
 	}
 }
